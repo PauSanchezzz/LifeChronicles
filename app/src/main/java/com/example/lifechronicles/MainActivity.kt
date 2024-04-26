6f102c90eef1a7ac92d338bb7496c13f46953cfb
PaulaSanchez <pausanchezt12@gmail.com>
2024-04-25 21:32:37 -0500

    log in screen added

--- app/src/main/java/com/example/lifechronicles/MainActivity.kt
+++ app/src/main/java/com/example/lifechronicles/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.lifechronicles.ui.navigation.AppNavigation
+import com.example.lifechronicles.ui.screens.LogInScreen
 import com.example.lifechronicles.ui.screens.SignUpScreen
 import com.example.lifechronicles.ui.screens.WelcomeScreen
 import com.example.lifechronicles.ui.theme.LifeChroniclesTheme
@@ -13,7 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LifeChroniclesTheme {
-                SignUpScreen()
+                LogInScreen()
             }
             //AppNavigation()
          // WelcomeScreen()
