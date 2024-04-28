633c5b2398a7f1a86b1f9f103b86d3150bb449a0
SnakyDH <skdanielg5@gmail.com>
2024-04-28 16:09:52 -0500

    feat: add eventList screen

---
+++ app/src/main/java/com/example/lifechronicles/ui/screens/EventsListScreen.kt
@@ -0,0 +1,70 @@
+package com.example.lifechronicles.ui.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.lifechronicles.R
+import com.example.lifechronicles.ui.components.EventCard
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EventsListScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        modifier = Modifier.clickable {
+                            navController.popBackStack()
+                        },
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "arrow back"
+                    )
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    Text(stringResource(id = R.string.events))
+                }
+            })
+        },
+        content = {
+            LazyColumn(
+                modifier = Modifier.padding(
+                    vertical = it.calculateTopPadding() + 10.dp,
+                    horizontal = 16.dp
+                ),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(15.dp),
+            ) {
+                items(10) {
+                    EventCard(
+                        imageUrl = "https://i.redd.it/1f8jhybnge6c1.jpeg",
+                        name = "Event",
+                        location = "Location",
+                        rating = 5.0,
+                        prices = listOf("$10", "$20")
+                    )
+                }
+            }
+
+        }
+    )
+}
