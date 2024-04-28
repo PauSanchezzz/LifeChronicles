6678076fe8c0ef28e2c5733d1b797d199cc5144d
SnakyDH <skdanielg5@gmail.com>
2024-04-28 15:01:29 -0500

    feat: add Home Navigation

--- app/src/main/java/com/example/lifechronicles/ui/navigation/AppScreens.kt
+++ app/src/main/java/com/example/lifechronicles/ui/navigation/AppScreens.kt
@@ -3,4 +3,5 @@ package com.example.lifechronicles.ui.navigation
 sealed class AppScreens(val route: String){
     data object  LogInScreen : AppScreens("login_screen")
     data object  SignUpScreen : AppScreens("signup_screen")
+    data object  HomeScreen : AppScreens("home_screen")
 }
