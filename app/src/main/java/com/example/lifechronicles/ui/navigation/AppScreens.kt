6fde89a2b44f29324822fc8d0d239c4f138e9118
PaulaSanchez <pausanchezt12@gmail.com>
2024-05-01 12:40:41 -0500

    new routes added

--- app/src/main/java/com/example/lifechronicles/ui/navigation/AppScreens.kt
+++ app/src/main/java/com/example/lifechronicles/ui/navigation/AppScreens.kt
@@ -6,4 +6,7 @@ sealed class AppScreens(val route: String) {
     data object HomeScreen : AppScreens("home_screen")
     data object EventsListScreen : AppScreens("events_list_screen")
     data object EventDetailScreen : AppScreens("event_detail_screen")
+    data object ChatListScreen : AppScreens("chat_list_screen")
+    data object ChatScreen : AppScreens("chat_screen")
+    data object WelcomeScreen : AppScreens("welcome_screen")
 }
